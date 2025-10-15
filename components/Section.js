69989c2cc6c3b0797678e3ebc82cfcdb0dd559fb
Samuel Whitford <Samw120586@gmail.com>
2025-10-15 13:46:02 -0400

    Project 8

---
+++ components/Section.js
@@ -0,0 +1,19 @@
+class Section {
+ constructor({ items, renderer, containerSelector }) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+ }
+
+    renderItems() {
+        this._items.forEach((item) => {
+            this.renderItems(item);
+        });
+    }
+
+    addItem(element) {
+        
+    }
+}
+
+export default Section;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,22 @@ import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from '../components/FormValidator.js';
+import Section from '../components/Section.js';
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
+
+const section = new Section({
+  items: [initialTodos],
+  renderer: () => {
+    generateTodo(item);
+    todosList.append(todo);
+  },
+    containerSelector: ".todos__list"
+});
 
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
@@ -52,6 +62,8 @@ initialTodos.forEach((item) => {
   const todo = generateTodo(item);
   todosList.append(todo);
 });
+
+
 
 const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
 newTodoValidator.enableValidation();
