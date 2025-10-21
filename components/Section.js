055bb38b62d5db07d47b2a25c462a8519d645fa1
Samuel Whitford <Samw120586@gmail.com>
2025-10-21 17:23:46 -0400

    Project 8 popup.js

--- components/Section.js
+++ components/Section.js
@@ -1,5 +1,19 @@
 class Section {
-    constructor(items, renderer, containerSelector) {
-        
+    constructor({ items, renderer, containerSelector}) {
+        this._items = items;
+        this._renderer = renderer;
+        this._container = document.querySelector(containerSelector);
+    }
+
+    renderItems() {
+        this._items.array.forEach((item) => {
+            renderer(item)
+        });
+    }
+
+    addItem(element) {
+        this._container.append(todo)
     }
 }
+
+export default Section;
