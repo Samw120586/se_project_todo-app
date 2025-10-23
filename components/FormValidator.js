3d77dc8d4d2cf37ca30b12f20092b59c73260901
Samuel Whitford <Samw120586@gmail.com>
2025-10-23 18:36:10 -0400

    Project 8 changes

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -11,7 +11,7 @@ class FormValidator {
 
     resetValidation() {
     this._toggleButtonState();
-    this_formEl.reset();
+    this._formEl.reset();
     }
  
     _showInputError(inputElement, errorMessage) {
